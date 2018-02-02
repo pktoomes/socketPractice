cf4a429fa0f1cd213fcb741fb55746e4d1ca4047
Patrick Toomes <pktoomes@g.cofc.edu>
2018-02-02 01:14:55 -0500

    added server socket

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 
     public static void main(String[] args) {
 	// write your code here
-        
+        try{
+            ServerSocket ss = new ServerSocket(9800);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
